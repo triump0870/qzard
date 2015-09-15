b811ac6ac904b2766562b2c249f7fb35e0dc2047
Rohan Roy <b4you0870@gmail.com>
2015-09-16 01:39:28 +0530

    New settings

---
+++ src/qzard/settings/staging.py
@@ -0,0 +1,4 @@
+# In staging set the environment variable like this:
+#    DJANGO_SETTINGS_MODULE=qzard.settings.staging
+from .base import *             # NOQA
+import logging.config
---
+++ src/qzard/settings/test.py
@@ -0,0 +1,4 @@
+# In test set the environment variable like this:
+#    DJANGO_SETTINGS_MODULE=qzard.settings.test
+from .base import *             # NOQA
+import logging.config
